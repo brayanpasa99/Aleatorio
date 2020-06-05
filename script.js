a20f2c86b89ad31589eb6f5b5850f5bfc215b722
brayanpasa99 <42362278+brayanpasa99@users.noreply.github.com>
2020-06-05 03:21:30 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-console.log("Hola mundo");
-let datoUsuario;
-function getdatoUsuario(){
+function holaMundo()
+{
+    console.log("Hola mundo");
+}
+var datoUsuario;
+function getdatoUsuario()
+{
     datoUsuario = document.getElementById("numU").value;
-    console.log("El valor del dato: " + datoUsuario);
-    
+    console.log("El valor del dato: " + datoUsuario);  
+    generaAleatorios();
+}
+
+function generaAleatorios()
+{
+    var ndatoUsuario = datoUsuario*-1;
+    for(var i=0; i<20; i++){
+        console.log(Math.floor(Math.random() * ( datoUsuario - ndatoUsuario + 1 ) + ndatoUsuario ));
+    }
+
 }
 

1cacb3ce152bed4f0d291196008cb1aab93a9949
brayanpasa99 <42362278+brayanpasa99@users.noreply.github.com>
2020-06-05 02:00:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hola mundo");
+let datoUsuario;
+function getdatoUsuario(){
+    datoUsuario = document.getElementById("numU").value;
+    console.log("El valor del dato: " + datoUsuario);
+    
+}
+

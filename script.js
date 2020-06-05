00355f4fc60acb92b4b32cd554397e0d41314cf4
brayanpasa99 <42362278+brayanpasa99@users.noreply.github.com>
2020-06-05 08:08:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function holaMundo()
     console.log("Hola mundo");
 }
 var datoUsuario;
+var lista;
+var camposLista = [];
 function getdatoUsuario()
 {
     datoUsuario = document.getElementById("numU").value;
@@ -12,10 +14,19 @@ function getdatoUsuario()
 
 function generaAleatorios()
 {
+    lista = document.getElementById("lAleatorios"); 
     var ndatoUsuario = datoUsuario*-1;
     for(var i=0; i<20; i++){
-        console.log(Math.floor(Math.random() * ( datoUsuario - ndatoUsuario + 1 ) + ndatoUsuario ));
+        if(lista.childNodes.length <= 20){
+            var newline = document.createElement("li");
+            newline.innerHTML = Math.floor(Math.random() * ( datoUsuario - ndatoUsuario + 1 ) + ndatoUsuario );
+            camposLista.push(newline);
+            lista.appendChild(newline);
+        } else {
+            camposLista[i].innerHTML = Math.floor(Math.random() * ( datoUsuario - ndatoUsuario + 1 ) + ndatoUsuario );    
+        }
     }
 
 }
+
 
